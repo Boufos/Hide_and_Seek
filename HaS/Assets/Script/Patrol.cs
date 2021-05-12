9a5c5eff989c9c5ed7cee3e662044bef95240029
Boufos <boufos@mail.ru>
2021-05-12 19:31:04 +0400

    UpgradeScene

--- HaS/Assets/Script/Patrol.cs
+++ HaS/Assets/Script/Patrol.cs
@@ -6,10 +6,12 @@ public class Patrol : MonoBehaviour
 {
     [SerializeField] Vector3[] transformPoints;
     public float speed;
+    public float rotate;
     int i = 0;
 
     void Update()
     {
+        
         if(i>transformPoints.Length-1)
         {
             i=0;
@@ -17,8 +19,19 @@ public class Patrol : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, transformPoints[i], speed*Time.deltaTime);
         if(Vector3.Distance(transform.position, transformPoints[i])<0.2f)
         {
+           transform.Rotate(new Vector3(0, rotate, 0));
             i++;
         }
        
     }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("POPAVCYA");
+        }
+    }
+
 }
